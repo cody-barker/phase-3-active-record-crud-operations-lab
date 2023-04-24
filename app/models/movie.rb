1646c52858e5fdb3d7a6e1992b03db2d4f4241b5
Cody <codybarker.or@gmail.com>
2023-04-24 15:57:16 -0700

    s

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,31 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        self.create(:title => title)
+    end
+
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(attr)
+        self.find_by(attr)
+    end
+
+    def self.find_movies_after_2002
+        self.find_by :release_date
+    end
+
 end
